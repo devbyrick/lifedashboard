dd9ccf649bef281451408427b96f79e7e82c2020
ricardo pestana <oricardopestana@gmail.com>
2023-05-24 23:26:17 +0100

    chore: added TailwindCSS and DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="btn text-red-400">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</div>
