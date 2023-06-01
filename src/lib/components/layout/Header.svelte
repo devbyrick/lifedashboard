672a89c585f30227a6e3e3e31160a7219bf6e872
ricardo pestana <oricardopestana@gmail.com>
2023-06-01 18:16:06 +0100

    feat: header > notification: added notification icon

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-
 import {Routes} from "../../constants/routes";
+import IconBell from "../icons/IconBell.svelte";
 </script>
 
-<div class="navbar bg-neutral text-neutral-content">
+<div class="navbar bg-neutral text-neutral-content justify-between">
     <a href={Routes.Dashboard} class="btn btn-ghost normal-case text-xl">Dashboard</a>
+    <div class="pr-5">
+        <div class="indicator">
+            <span class="indicator-item badge badge-secondary p-0 w-3 h-3"></span>
+            <IconBell class="w-6 h-6"></IconBell>
+        </div>
+    </div>
 </div>
