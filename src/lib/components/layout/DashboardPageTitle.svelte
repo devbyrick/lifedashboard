7872d2383bd6d671d5f86c8c89c9b7bb2be57d27
ricardo pestana <oricardopestana@gmail.com>
2023-06-08 00:26:14 +0100

    feat: added dashboard page title component

---
+++ src/lib/components/layout/DashboardPageTitle.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import BackButton from "../ui/BackButton.svelte";
+
+    export let pageTitle = 'Home';
+</script>
+
+<div class="flex">
+    <BackButton />
+    {pageTitle}
+</div>
